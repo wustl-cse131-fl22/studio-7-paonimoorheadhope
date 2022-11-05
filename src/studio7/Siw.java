d1b877c12090542d3cbf1a819bacda0484e1ea62
benpaoni <benpaoni@bens-mbp.wusm-wifi.wucon.wustl.edu>
2022-11-05 18:56:05 -0500

    studio 7

--- src/studio7/Siw.java
+++ src/studio7/Siw.java
@@ -1,5 +1,30 @@
 package studio7;
 
-public class Siw {
+import java.util.Scanner;
 
+import edu.princeton.cs.introcs.StdDraw;
+
+public class Die {
+
+	private int n = 0;
+	Scanner in = new Scanner(System.in);
+
+	public Die(int x) {
+		this.n = x;
+	}
+
+	public void setN(int n) {
+		this.n = n;
+	}
+
+	public int getN(int n) {
+		this.n = n;
+		return (int) Math.random()*this.n;
+	}
+	
+
+	public void main(String[] args) {
+		System.print(getN(n));
+	}
+	
 }
